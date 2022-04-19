cb8b6d2e6fc1d78b4f407fd3514402924a7c6324
Gina Hobbs <ginamariehobbs@gmail.com>
2022-04-19 20:11:36 +0000

    code pushed

--- ops11_network.py
+++ ops11_network.py
@@ -6,17 +6,43 @@
 #Declaration of variables: 
 #Declaration of functions (if used):
 
-#import libraries
-import sys
-from scapy.all import sr1,IP,ICMP,TCP
+# Code taken from:
+# https://thepacketgeek.com/scapy/building-network-tools/part-10/
+import random
+from scapy.all import ICMP, IP, sr1, TCP, sr
 
-#define target host and TCP port to scan
-
+# Define end host and TCP port range
 host = "scanme.nmap.org"
-port_range = 22
-src_port = 22
-dst_port = 22
+#host = "192.168.1.212"
+port_range = [22, 23, 80, 443, 3389]
 
-response = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port),timeout=1,verbose=0)
+# Send SYN with random Src Port for each Dst port
+for dst_port in port_range:
+    src_port = random.randint(1025,65534)
+    resp = sr1(
+        IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,
+        verbose=0,
+    )
 
-print(response)
+    if resp is None:
+        print(f"{host}:{dst_port} is filtered (silently dropped).")
+
+    elif(resp.haslayer(TCP)):
+        if(resp.getlayer(TCP).flags == 0x12):
+            # Send a gratuitous RST to close the connection
+            send_rst = sr(
+                IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags='R'),
+                timeout=1,
+                verbose=0,
+            )
+            print(f"{host}:{dst_port} is open.")
+
+        elif (resp.getlayer(TCP).flags == 0x14):
+            print(f"{host}:{dst_port} is closed.")
+
+    elif(resp.haslayer(ICMP)):
+        if(
+            int(resp.getlayer(ICMP).type) == 3 and
+            int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]
+        ):
+            print(f"{host}:{dst_port} is filtered (silently dropped).")
